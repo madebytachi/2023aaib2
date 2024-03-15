ec68c9f52bc1ad5cd35f96989b5ec455eef967a3
tachibleu <tachibleu.org@gmail.com>
2024-03-15 11:48:59 +0800

    week04

---
+++ week04/week04-3.py
@@ -0,0 +1,7 @@
+#week04-3 python版 要試 for迴圈 比較複新的參數
+#for i in range(1,10): #1...9
+for i in range(1,10-1,+1):
+  print('i是', i, end=' ')
+print() #跳行
+for i in range(9, 1-1, -1): #9...1
+  print('i是', i, end=' ')
